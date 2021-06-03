2ed92ec128a43918825615b08aa2f1fd0d96ec18
A601-20 <tnk.dat.class@gmail.com>
2021-06-03 11:05:19 +0900

    変更

--- Assets/Hensu.cs
+++ Assets/Hensu.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Hensu : MonoBehaviour
 {
+    int a = 10;
+    
     // Start is called before the first frame update
     public void Proc()
     {
-        int a = 10;
+
         float fw = 3.14f;
         string str = "こんにちは";
         bool bl = true;
@@ -18,6 +20,10 @@ public class Hensu : MonoBehaviour
         Debug.Log(bl);
     }
 
+    public void Proc2()
+    {
+        Debug.Log(a);
+    }
 
     // Update is called once per frame
     void Update()
